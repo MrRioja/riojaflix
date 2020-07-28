0c2f8ce0d4aad9cb76a66a003e6c44cad4cbe65c
Luiz Rioja <lulyrioja@gmail.com>
2020-07-28 18:31:49 -0300

    Adicionando roteamento na aplicação e incrementando carrossel

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/Imagens/Logo.png';
 import './Menu.css';
 import Button from '../Button';
@@ -6,10 +7,10 @@ import Button from '../Button';
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="Logo" />
-      </a>
-      <Button as="a" className="Button" href="/">Novo vídeo</Button>
+      </Link>
+      <Button as={Link} className="Button" to="/cadastro/video">Novo vídeo</Button>
     </nav>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import CadastroVideo from './Pages/Cadastro/Video';
+import CadastroCategoria from './Pages/Cadastro/Categoria';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/cadastro/video" component={CadastroVideo} />
+      <Route exact path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={() => <h1>Deu merda</h1>} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
